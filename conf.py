f971ab99fe980d2b512c26c4a63e1e4c6be22696
Tetsuo Koyama <tkoyama010@gmail.com>
2020-08-06 01:24:02 +0900

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,49 @@
+# basedir is set by <lang>/conf.py
+"""
+Use "-D language=<LANG>" option to build a localized pyvista document.
+For example::
+
+    sphinx-build -D language=ja -b html . _build/html
+
+This conf.py do:
+
+- Specify `locale_dirs` and `gettext_compact`.
+- Overrides source directory as 'pyvista/docs`.
+
+"""
+import os
+from sphinx.util.pycompat import execfile_
+
+basedir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "pyvista/docs/source/pyvista"
+)
+
+execfile_(os.path.join(basedir, "conf.py"), globals())
+
+locale_dirs = [os.path.join(basedir, "../../locale/")]
+
+
+def setup(app):
+    from sphinx.ext.autodoc import cut_lines
+    from sphinx.util.docfields import GroupedField
+
+    app.srcdir = basedir
+    app.confdir = app.srcdir
+    app.connect("autodoc-process-docstring", cut_lines(4, what=["module"]))
+    app.add_object_type(
+        "confval",
+        "confval",
+        objname="configuration value",
+        indextemplate="pair: %s; configuration value",
+    )
+    fdesc = GroupedField(
+        "parameter", label="Parameters", names=["param"], can_collapse=True
+    )
+
+    # workaround for RTD
+    from sphinx.util import logging
+
+    logger = logging.getLogger(__name__)
+    app.info = lambda *args, **kwargs: logger.info(*args, **kwargs)
+    app.warn = lambda *args, **kwargs: logger.warning(*args, **kwargs)
+    app.debug = lambda *args, **kwargs: logger.debug(*args, **kwargs)
