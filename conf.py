5da8f90cf9541b13718e8f10b1ad484f2919a15d
Tetsuo Koyama <tkoyama010@gmail.com>
2020-08-06 02:33:08 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,25 +14,6 @@ This conf.py do:
 import os
 from sphinx.util.pycompat import execfile_
 
-check_call(
-    [
-        "apt-get",
-        "install",
-        "-y",
-        "--no-install-recommends",
-        "libgl1-mesa-dev",
-        "xvfb",
-        "ffmpeg",
-    ],
-    stdout=open(os.devnull, "wb"),
-    stderr=STDOUT,
-)
-os.environ["PYVISTA_VIRTUAL_DISPLAY"] = true
-os.environ["PYVISTA_OFF_SCREEN"] = true
-os.environ["PYVISTA_USE_PANEL"] = true
-os.environ["PYVISTA_PLOT_THEME"] = document
-os.environ["PYVISTA_AUTO_CLOSE"] = false
-
 basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "pyvista/docs")
 
 execfile_(os.path.join(basedir, "conf.py"), globals())
