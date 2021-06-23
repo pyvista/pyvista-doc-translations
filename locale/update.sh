980e148313e8708790019e37ab767bc55bf79029
Tetsuo Koyama <tkoyama010@gmail.com>
2021-06-23 14:19:10 +0900

    🚚 Fix path of CI (#96)
    * 🚚 Fix path of CI
    
    * 🚚 Fix path of CI
    
    * 💚 Update submodule before moving
    
    * 📛 Master -> Main

--- locale/update.sh
+++ locale/update.sh
@@ -12,7 +12,7 @@ LOCAL_PYTHON_PATH="/home/runner/work/.venv/bin"
 cd `dirname $0`
 $LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../pyvista/docs pot
+$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../pyvista/doc pot
 $LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 $LOCAL_PYTHON_PATH/tx push -s --skip
