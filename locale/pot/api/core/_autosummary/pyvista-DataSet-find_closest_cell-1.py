1a04d97369b09ddebf275c58c9d30f9a7c16ee1e
tkoyama010/pyvista-doc-translations <tkoyama010/pyvista-doc-translations>
2021-11-02 08:53:29 +0000

    [ci skip] 4077361401?check_suite_focus=true https://github.com/tkoyama010/pyvista-doc-translations/runs/4077361401?check_suite_focus=true

--- locale/pot/api/core/_autosummary/pyvista-DataSet-find_closest_cell-1.py
+++ locale/pot/api/core/_autosummary/pyvista-DataSet-find_closest_cell-1.py
@@ -1,19 +1,28 @@
-# Find nearest cell to a point on a sphere
+# Find nearest cell to a point on a sphere, centered on the
+# origin.
 #
 import pyvista
 mesh = pyvista.Sphere()
 index = mesh.find_closest_cell([0, 0, 0.5])
 index
 # Expected:
-## 59
+## 30
 #
-# Find the nearest cells to several random points.  Note that
-# ``-1`` indicates that the locator was not able to find a
-# reasonably close cell.
+# Find the nearest cells to several random points that
+# are centered on the origin.
 #
 import numpy as np
-points = np.random.random((1000, 3))
+points = 2 * np.random.random((5000, 3)) - 1
 indices = mesh.find_closest_cell(points)
 indices.shape
 # Expected:
-## (1000,)
+## (5000,)
+#
+# The average position of all the randomly found cell centers should
+# be reasonably close to the origin.
+#
+cell_center_mesh = mesh.cell_centers()
+avg_pos = cell_center_mesh.points[indices, :].mean(axis=0)
+np.linalg.norm(avg_pos) < 0.02
+# Expected:
+## True
--- locale/pot/api/examples/_autosummary/pyvista-demos-demos-glyphs-1.py
+++ locale/pot/api/examples/_autosummary/pyvista-demos-demos-glyphs-1.py
@@ -1,2 +1,3 @@
 from pyvista import demos
 mesh = demos.glyphs()
+mesh.plot()
