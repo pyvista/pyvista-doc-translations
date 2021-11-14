70e5b72012cf92d2d4365feb83e208adb439ed06
Tetsuo Koyama <tkoyama010@gmail.com>
2021-11-15 08:29:26 +0900

    Delete locale/pot/user-guide directory

--- locale/pot/user-guide/jupyter/itk_plotting-1.py
+++
@@ -1,6 +0,0 @@
-import pyvista
-mesh = pyvista.Sphere()
-pl = pyvista.PlotterITK()  # doctest:+SKIP
-pl.add_mesh(mesh, color='w')  # doctest:+SKIP
-pl.background_color = 'k'  # doctest:+SKIP
-pl.show()  # doctest:+SKIP
--- locale/pot/user-guide/themes-4.py
+++
@@ -1,1 +0,0 @@
-dragon.plot(cpos='xy', color='red', background='white', show_axes=False)
