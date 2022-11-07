304aed0ea11ecbce69d7f3dfa1395ff53ff43bc8
Tetsuo Koyama <tkoyama010@gmail.com>
2022-11-08 06:56:19 +0900

    👷 Add .pre-commit-config.yaml (#194)
    * 👷 Add .pre-commit-config.yaml
    
    * Create pre-commit-update.yml
    
    * Create style.yml
    
    * Add extrude
    
    * Remove flake8
    
    * Fix by pre-commit

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'pyvista-doc-translations'
-copyright = '2021, Tetsuo Koyama'
-author = 'Tetsuo Koyama'
+project = "pyvista-doc-translations"
+copyright = "2021, Tetsuo Koyama"
+author = "Tetsuo Koyama"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,16 +27,15 @@ author = 'Tetsuo Koyama'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,15 +43,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-locale_dirs = ["."]   # path is example but recommended.
-gettext_compact = False     # optional.
+locale_dirs = ["."]  # path is example but recommended.
+gettext_compact = False  # optional.
 
 # To specify names to enable gettext extracting and translation applying for i18n additionally. You can specify below names:
-gettext_additional_targets = ['raw']
+gettext_additional_targets = ["raw"]
